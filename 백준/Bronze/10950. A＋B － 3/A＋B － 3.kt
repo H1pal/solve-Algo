f31841533555d06a3468f17d8f2fb3dadf88353f
H1pal <gmlvkf6666@gmail.com>
2026-04-02 19:29:03 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14612 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.kt
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.kt
@@ -1,8 +1,7 @@
 fun main() {
     val amount = readln().toInt()
 
-    for (i in 1..amount) {
-        val (n1, n2) = readln().split(" ").map { it.toInt() }
-        println(n1 + n2)
+    for (item in 1..amount) {
+        println(readln().split(" ").sumOf { it.toInt() })
     }
 }
