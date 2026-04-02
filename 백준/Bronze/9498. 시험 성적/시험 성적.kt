494f1f8c84cc9eac0fbeec0459b1ba83476b886b
H1pal <gmlvkf6666@gmail.com>
2026-04-02 12:31:02 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 12332 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.kt
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -2,11 +2,11 @@ fun main() {
     val num = readln().toInt()
 
     println(
-        when {
-            num >= 90 -> "A"
-            num >= 80 -> "B"
-            num >= 70 -> "C"
-            num >= 60 -> "D"
+        when (num){
+            in 90..100 -> "A"
+            in 80..89 -> "B"
+            in 70..79 -> "C"
+            in 60..69 -> "D"
             else -> "F"
         }
     )
