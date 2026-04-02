f3778283deb88e4d350a799d73cdb5d7be1c8d6f
H1pal <gmlvkf6666@gmail.com>
2026-04-02 10:53:43 +0900

    [Bronze V] Title: 윤년, Time: 76 ms, Memory: 12368 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.kt
+++ 백준/Bronze/2753. 윤년/윤년.kt
@@ -1,12 +1,6 @@
 fun main() {
     val year = readln().toInt()
-
-    println(
-        when {
-            year % 400 == 0-> 1
-            year % 100 == 0 -> 0
-            year % 4 == 0 -> 1
-            else -> 0
-        }
-    )
+    
+    println(if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0) 1 else 0)
+        
 }
