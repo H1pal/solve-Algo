14674ebf2d73f6eec11398d7e85bcf8d4768d0ba
H1pal <gmlvkf6666@gmail.com>
2026-04-03 10:47:33 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 20336 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.kt
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.kt
@@ -2,12 +2,6 @@ fun main() {
     val num = readln().toInt()
 
     for (i in 1..num) {
-        for (j in   1..(num-i)) {
-            print(" ")
-        }
-        for (j in 1..i) {
-            print("*")
-        }
-        println()
+        println("*".repeat(i).padStart(num))
     }
 }
