e6c99298ce41ad056cea7211e6e0ada48dffc4cc
H1pal <gmlvkf6666@gmail.com>
2026-04-11 19:24:43 +0900

    [Bronze II] Title: 분산처리, Time: 1020 ms, Memory: 23828 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val cs = readln().toInt()
+    var total = 1
+
+    generateSequence { readln().split(" ").map { it.toInt() }}
+        .take(cs)
+        .forEach { (num, amo) -> repeat(amo) { total = (total * num) % 10 }
+            .also {
+                println(if (total % 10 != 0) total % 10 else 10) }; total = 1 }
+
+//    repeat(cs) {
+//        val (num, amo) =  readln().split(" ").map { it.toInt() }
+//        repeat(amo) {total *= num}.also { println(if (total % 10 == 0) 10 else total % 10); total = 1 }
+//    }
+}
