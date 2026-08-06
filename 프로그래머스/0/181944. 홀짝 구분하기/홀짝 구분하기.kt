8ba53aa3001efa1b0671be23cc7a7ca0c46a0b0d
H1pal <gmlvkf6666@gmail.com>
2026-08-07 06:04:33 +0900

    [level 0] Title: 홀짝 구분하기, Time: 68.18 ms, Memory: 51.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val a = readln().toInt()
+    
+    
+    if (a % 2 == 1) {
+        println("${a} is odd")
+    } else {
+        println("${a} is even")
+    }
+    
+    
+}
