5e5682378f7d7c8ee9c435bffc567d9b16671d15
H1pal <gmlvkf6666@gmail.com>
2026-08-07 18:31:58 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 5.90 ms, Memory: 60.4 MB -BaekjoonHub

---
+++ 프로그래머스/Kotlin/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String, index_list: IntArray): String {
+        var answer: String = ""
+        index_list.forEach { answer += my_string[it] }
+        return answer
+    }
+}
