6572d7a5e9f7adeaaba25e8e29dec0564c69b1f3
H1pal <gmlvkf6666@gmail.com>
2026-08-11 15:14:46 +0900

    [level 0] Title: 카운트 업, Time: 7.60 ms, Memory: 59.2 MB -BaekjoonHub

---
+++ 프로그래머스/Kotlin/0/181920. 카운트 업/카운트 업.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(start_num: Int, end_num: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        answer = (start_num..end_num).toList().toIntArray()
+        return answer
+    }
+}
