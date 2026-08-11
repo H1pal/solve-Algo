1d568195ee04dd9cdc06eef2851a2099f8bf508a
H1pal <gmlvkf6666@gmail.com>
2026-08-11 17:40:08 +0900

    [level 0] Title: 문자열 정수의 합, Time: 1.30 ms, Memory: 57.9 MB -BaekjoonHub

---
+++ 프로그래머스/Kotlin/0/181849. 문자열 정수의 합/문자열 정수의 합.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num_str: String): Int {
+        var answer: Int = 0
+        num_str.forEach {
+            answer += it.digitToInt()
+        }
+        return answer
+    }
+}
