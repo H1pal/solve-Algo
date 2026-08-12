f567884a31be7f756a045a94b7bdf449a2e32511
H1pal <gmlvkf6666@gmail.com>
2026-08-12 18:11:21 +0900

    [level 0] Title: 배열 만들기 3, Time: 21.91 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/Kotlin/0/181895. 배열 만들기 3/배열 만들기 3.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(arr: IntArray, intervals: Array<IntArray>): IntArray {
+        var answer: IntArray = intArrayOf()
+        answer = (arr.slice(intervals[0][0]..intervals[0][1]) + arr.slice(intervals[1][0]..intervals[1][1])).toIntArray()
+        return answer
+    }
+}
