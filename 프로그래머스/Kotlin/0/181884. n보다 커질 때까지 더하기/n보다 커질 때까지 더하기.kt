bfd38dc0e522db2b096d0f6b5e7d1d4a9132a817
H1pal <gmlvkf6666@gmail.com>
2026-08-12 17:29:32 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 59.3 MB -BaekjoonHub

---
+++ 프로그래머스/Kotlin/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(numbers: IntArray, n: Int): Int {
+        var answer: Int = 0
+        for (num in numbers) {
+            answer += num
+            if (answer > n) break
+        }
+        
+        return answer
+    }
+}
