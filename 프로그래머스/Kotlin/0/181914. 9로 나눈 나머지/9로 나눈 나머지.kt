f697d835e184b0365abb70e904fb2f596aded9ba
H1pal <gmlvkf6666@gmail.com>
2026-08-13 11:23:19 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.48 ms, Memory: 59.4 MB -BaekjoonHub

---
+++ 프로그래머스/Kotlin/0/181914. 9로 나눈 나머지/9로 나눈 나머지.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(number: String): Int {
+        var answer: Int = 0
+        number.forEach {
+            answer += it.digitToInt()
+        }
+        return answer % 9
+    }
+}
