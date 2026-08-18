4ccdf2a8d6280b5375798cbabdc067e6ac6ab9bd
H1pal <gmlvkf6666@gmail.com>
2026-08-18 12:43:20 +0900

    [level 0] Title: 글자 지우기, Time: 9.75 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/Kotlin/0/181900. 글자 지우기/글자 지우기.kt
+++ 프로그래머스/Kotlin/0/181900. 글자 지우기/글자 지우기.kt
@@ -1,8 +1,9 @@
 class Solution {
     fun solution(my_string: String, indices: IntArray): String {
         var answer: String = ""
-        answer = my_string.filterIndexed { idx, _ ->
-            idx !in indices
+        answer = my_string
+        indices.forEach {
+            answer = answer.take(it) + answer.drop(it+1)
         }
         return answer
     }
