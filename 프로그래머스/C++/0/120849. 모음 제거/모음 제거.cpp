9f972322dde22d5fec51774bebbd652045fb67ce
H1pal <gmlvkf6666@gmail.com>
2026-09-26 00:49:12 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/C++/0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for (const char i: my_string) {
+        if (!(i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u')) answer += i;
+    }
+    
+    return answer;
+}
