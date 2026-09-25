e93c0cc15c76af8c9a6901366e1db26914f67e96
H1pal <gmlvkf6666@gmail.com>
2026-09-26 00:39:41 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/C++/0/120848. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    
+    while (n > answer) {
+        n /= ++answer;
+    }
+    
+    return answer;
+}
