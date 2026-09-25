b21fc69c700ce2ad2249b5f5ee1d935f2f242751
H1pal <gmlvkf6666@gmail.com>
2026-09-26 01:05:02 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 4.79 MB -BaekjoonHub

---
+++ 프로그래머스/C++/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    for (const char i: my_string) {
+        if (i >= '0' && i <= '9') answer += i - '0';
+    }
+    return answer;
+}
