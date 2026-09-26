cda7f3366f6d0ec26b1e56ca4e2238a42b5849d7
H1pal <gmlvkf6666@gmail.com>
2026-09-26 18:38:15 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/C++/0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+    string answer(my_string.begin(), my_string.end());
+    char temp = answer[num1];
+    answer[num1] = answer[num2];
+    answer[num2] = temp;
+    return answer;
+}
