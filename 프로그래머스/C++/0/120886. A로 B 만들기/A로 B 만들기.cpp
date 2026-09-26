12b57bf1792e51af77ccacaa2c92b5a147fbedbd
H1pal <gmlvkf6666@gmail.com>
2026-09-26 22:55:16 +0900

    [level 0] Title: A로 B 만들기, Time: 0.06 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/C++/0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(string before, string after) {
+    int answer = 0;
+    sort(before.begin(), before.end());
+    sort(after.begin(), after.end());
+    answer = before == after;
+    return answer;
+}
